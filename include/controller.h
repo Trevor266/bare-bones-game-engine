086c3bfbea24fe9a9d7c532776e9f0a567a7c7de
trevor266 <trevorhart26@gmail.com>
2026-06-06 16:40:56 -0500

    bb-3-Significantly cleaned up win32 layer and separation of declarations and definitions, added much better debug logging, corrected codepoint to be the actual unicode character.

--- include/controller.h
+++ include/controller.h
@@ -1,4 +1,31 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+/*
+VK_GAMEPAD_A                        0xC3    Gamepad A button
+VK_GAMEPAD_B                        0xC4    Gamepad B button
+VK_GAMEPAD_X                        0xC5    Gamepad X button
+VK_GAMEPAD_Y                        0xC6    Gamepad Y button
+VK_GAMEPAD_RIGHT_SHOULDER           0xC7    Gamepad Right Shoulder button
+VK_GAMEPAD_LEFT_SHOULDER            0xC8    Gamepad Left Shoulder button
+VK_GAMEPAD_LEFT_TRIGGER             0xC9    Gamepad Left Trigger button
+VK_GAMEPAD_RIGHT_TRIGGER            0xCA    Gamepad Right Trigger button
+VK_GAMEPAD_DPAD_UP                  0xCB    Gamepad D-pad Up button
+VK_GAMEPAD_DPAD_DOWN                0xCC    Gamepad D-pad Down button
+VK_GAMEPAD_DPAD_LEFT                0xCD    Gamepad D-pad Left button
+VK_GAMEPAD_DPAD_RIGHT               0xCE    Gamepad D-pad Right button
+VK_GAMEPAD_MENU                     0xCF    Gamepad Menu/Start button
+VK_GAMEPAD_VIEW                     0xD0    Gamepad View/Back button
+VK_GAMEPAD_LEFT_THUMBSTICK_BUTTON   0xD1    Gamepad Left Thumbstick button
+VK_GAMEPAD_RIGHT_THUMBSTICK_BUTTON  0xD2    Gamepad Right Thumbstick button
+VK_GAMEPAD_LEFT_THUMBSTICK_UP       0xD3    Gamepad Left Thumbstick up
+VK_GAMEPAD_LEFT_THUMBSTICK_DOWN     0xD4    Gamepad Left Thumbstick down
+VK_GAMEPAD_LEFT_THUMBSTICK_RIGHT    0xD5    Gamepad Left Thumbstick right
+VK_GAMEPAD_LEFT_THUMBSTICK_LEFT     0xD6    Gamepad Left Thumbstick left
+VK_GAMEPAD_RIGHT_THUMBSTICK_UP      0xD7    Gamepad Right Thumbstick up
+VK_GAMEPAD_RIGHT_THUMBSTICK_DOWN    0xD8    Gamepad Right Thumbstick down
+VK_GAMEPAD_RIGHT_THUMBSTICK_RIGHT   0xD9    Gamepad Right Thumbstick right
+VK_GAMEPAD_RIGHT_THUMBSTICK_LEFT    0xDA    Gamepad Right Thumbstick left
+*/
+
 #endif
